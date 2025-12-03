c4a046882ea9e8e92095d4a82e2d99c0b0d759bc
Fadel Ahmad F <fadelahmadf@gmail.com>
2025-12-03 15:25:39 +0700

    Save changes before switching branch

--- part3-practice/index.js
+++ part3-practice/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 console.log(express)
 const app = express()
-const cors = require('cors')
+// const cors = require('cors')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -21,7 +21,7 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(requestLogger)
