ee66b6c976e9c297e817d43145160f41144872f3
Fadel Ahmad F <fadelahmadf@gmail.com>
2025-12-05 17:05:52 +0700

    add mongo.js file to connect to mongodb and install mongoose package

---
+++ part3/phonebookbackend/mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3){
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+
+
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fadelahmadf_db_user:${password}@cluster0.ndxvanv.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, {family: 4})
+
+const phoneSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Phone = mongoose.model('Phone', phoneSchema)
+
+console.log('sampe sini jalan')
+if (process.argv.length === 3){
+
+    Phone.find({}).then(persons => {
+
+        console.log('phonebook:') 
+        persons.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+
+        mongoose.connection.close()
+    })
+
+}
+
+// const phone = new Phone({
+//     name: process.argv[3],
+//     number: process.argv[4]
+// })
+
+// phone.save().then(result => {   
+//     console.log(`added ${result.name} number ${result.number} to phonebook`)
+//     mongoose.connection.close()
+// }
+// )
