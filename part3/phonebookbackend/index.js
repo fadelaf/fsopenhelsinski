cb8ee8ef23d8a80fe192bd0bb40bed4fb828175d
Fadel Ahmad F <fadelahmadf@gmail.com>
2025-12-12 10:33:09 +0700

    generate new dist frontend and create validators in models for name and number

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -233,6 +233,10 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   }
 
+  if (error.name === 'ValidationError'){
+    return response.status(400).send({ error: error.message })
+  }
+
   next(error)
 }
 
